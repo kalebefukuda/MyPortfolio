f3e2ff982dd7b021172eed54581dfbdcf269c5ba
Kalebe Fukuda <kalebefukudadeoliveira@gmail.com>
2023-08-04 17:14:53 -0300

    start portfolio

---
+++ src/compenents/home/Social.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+   <div className="home_social">
+    <a href="https://www.instagram.com/kalebefukuda_/" className="home_social-icon" target='_blank'>
+    <i class="uil uil-instagram"></i>
+    </a>
+
+    <a href="" className="home_social-icon" target='_blank'>
+    <i class="uil uil-dribbble"></i>
+    </a>
+
+    <a href="https://github.com/kalebefukuda" className="home_social-icon" target='_blank'>
+    <i class="uil uil-github-alt"></i>
+    </a>
+   </div>
+  )
+}
+
+export default Social
