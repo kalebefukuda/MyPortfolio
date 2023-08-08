d01aa3fa6db107f499a87dfdf0290f7834e2bf61
Kalebe Fukuda <kalebefukudadeoliveira@gmail.com>
2023-08-08 15:45:16 -0300

    teste deploy

--- src/compenents/header/Header.jsx
+++ src/compenents/header/Header.jsx
@@ -16,7 +16,7 @@ const Header = () => {
   return (
     <header className='header'>
         <nav className="nav container">
-            <a href="index.html" className="nav_logo">Kalebe Fukuda</a>
+            <a href="index.html" className="nav_logo">Kalebe Fukuda!</a>
 
            <div className={Toggle ? "nav_menu show-menu" : "nav_menu"}>
             <ul className="nav_list grid">
