a2215a6fef4b9c00e3bb1b5ecfb4ef29955ae21c
kalebe fukuda <kalebefukudadeoliveira@gmail.com>
2023-08-28 18:50:40 -0300

    add new project page links

--- src/compenents/work/Data.jsx
+++ src/compenents/work/Data.jsx
@@ -1,5 +1,5 @@
 import Work1 from "../../assets/work1.jpg";
-import Work2 from "../../assets/work2.jpg";
+import Work2 from "../../assets/work2.png";
 import Work3 from "../../assets/work3.png";
 import Work4 from "../../assets/work4.jpg";
 import Work5 from "../../assets/work5.jpg";
@@ -15,8 +15,9 @@ export const projectsData = [
     {
       id: 2,
       image: Work2,
-      title: "App movil",
-      category: "app",
+      title: "Page Links",
+      category: "web",
+      demoLink: "https://devlinks-rosy.vercel.app"
     },
     {
       id: 3,
