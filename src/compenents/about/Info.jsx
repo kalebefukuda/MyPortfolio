9b9e69480f2a0f81b5c381737344a84844c04d45
Kalebe Fukuda <kalebefukudadeoliveira@gmail.com>
2024-02-16 14:27:39 -0300

    change info in Info section

--- src/compenents/about/Info.jsx
+++ src/compenents/about/Info.jsx
@@ -7,14 +7,14 @@ const Info = () => {
         <i className="bx bx-award about_icon"></i>
 
         <h3 className="about_title"> Experience</h3>
-        <span className="about_subtitle">7 Months Studying</span>
+        <span className="about_subtitle">13 Months Studying</span>
     </div>
 
     <div className='about_box'>
         <i className="bx bx-briefcase-alt about_icon"></i>
 
         <h3 className="about_title">Working</h3>
-        <span className="about_subtitle">3 + Projects</span>
+        <span className="about_subtitle">4 + Projects</span>
     </div>
 
     <div className='about_box'>
