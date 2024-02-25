5bf0171bf6f7cc40a654bb10870109d114ad421c
Kalebe Fukuda <kalebefukudadeoliveira@gmail.com>
2024-02-25 00:12:52 -0300

    starting certificate section, add assets

--- src/App.js
+++ src/App.js
@@ -10,19 +10,22 @@ import Testimonials from './compenents/testimonials/Testimonials';
 import Contact from './compenents/contact/Contact';
 import Footer from './compenents/footer/Footer';
 import ScrollUp from './compenents/scrollup/ScrollUp';
+import Certificate from './compenents/certificate/Certificate';
 
 
 function App() {
   return (
     <>
       <Header />
-
+      
       <main className='main'>
+        
         <Home />
         <About />
         <Skills />
         <Services />
         <Qualification />
+        <Certificate />
         <Work />
         <Testimonials />
         <Contact />
